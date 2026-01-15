db8671649c4f2a725d44f60cec58ea886d2c003a
Jamie M Dowden-Duarte <152818382+Goober0415@users.noreply.github.com>
2026-01-15 09:27:19 -0700

    011520260927

---
+++ ParametersDemoDowdenDuarte.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parametersDemoDowdenDuarte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int testValue = 1;
+			Console.WriteLine("original value: {0}", testValue);
+			Test(testValue);
+			Console.WriteLine("return value: {0}", testValue);
+
+			int testValue2 = 2;
+			Console.WriteLine("original value: {0}", testValue2);
+			TestOut(out testValue2);
+			Console.WriteLine("return value: {0}", testValue2);
+		}
+		public static void Test(int aValue)
+		{
+			aValue = 111;
+			Console.WriteLine("value is {0}", aValue);
+		}
+
+		public static void TestOut(out int aValue)
+		{
+			aValue = 222;
+			Console.WriteLine("test out value {0}", aValue);
+		}
+	}
+}
+
