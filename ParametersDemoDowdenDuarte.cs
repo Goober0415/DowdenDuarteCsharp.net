90633d9a8959ad148aa674eca9fbad8c90c45998
Jamie M Dowden-Duarte <152818382+Goober0415@users.noreply.github.com>
2026-01-15 10:30:05 -0700

    011520261030

--- ParametersDemoDowdenDuarte.cs
+++ ParametersDemoDowdenDuarte.cs
@@ -32,6 +32,19 @@ namespace parametersDemoDowdenDuarte
 			Console.WriteLine("Calling testOptional with a parameter");
 			TestOptional(79);
 			Console.WriteLine("\n");
+
+			Console.WriteLine("Calling TestMultiple with parameters");
+			TestMultiple(1, 2, 3);
+			Console.WriteLine("\n");
+			Console.WriteLine("Calling TestMultiple without parameters");
+			TestMultiple();
+			Console.WriteLine("\n");
+			Console.WriteLine("Calling TestMultiple with first two parameters");
+			TestMultiple(4,5);
+			Console.WriteLine("\n");
+			Console.WriteLine("Calling TestMultiple with a name value");
+			TestMultiple(6,cValue:27);
+			Console.WriteLine("\n");
 		}
 
 		public static void Test(int aValue)
@@ -57,6 +70,12 @@ namespace parametersDemoDowdenDuarte
 		public static void TestOptional(int aValue = 444) //default value is 444
 		{
 			Console.WriteLine("test optional value {0}", aValue);
+		}
+
+		//Multiple parameters
+		public static void TestMultiple(int aValue = 123, int bValue = 234, int cValue = 345) //default value is 444
+		{
+			Console.WriteLine("test multiple values are {0} {1} {2}\n", aValue, bValue, cValue);
 		}
 	}
 }
