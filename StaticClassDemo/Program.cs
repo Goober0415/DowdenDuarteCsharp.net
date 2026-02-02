0c1d9b53ec7c80f7226e0c43f1c3a16f5f404e68
Jamie M Dowden-Duarte <152818382+Goober0415@users.noreply.github.com>
2026-02-02 11:03:29 -0700

    020220261103

---
+++ StaticClassDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace StaticClassDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string doAnother;
+
+			do
+			{
+				Student st = new Student();
+
+				string firstName = GetInput("First Name: ");
+				st.SetFirstName(firstName);
+
+				string lastName = GetInput("Last Name: ");
+				st.SetLastName(lastName);
+
+				// NEW CODE - Display majors from static array
+				Console.WriteLine("\nAvailable Majors:");
+				for (int i = 0; i < Student.majors.Length; i++)
+				{
+					Console.WriteLine((i + 1) + ". " + Student.majors[i]);
+				}
+
+				// Get major selection
+				int majorIndex = int.Parse(GetInput("Select Major (1-3): ")) - 1;
+				st.SetMajor(Student.majors[majorIndex]);
+
+				string studentNum = GetInput("Student Number: ");
+				st.SetStudentNumber(studentNum);
+
+				int score1 = int.Parse(GetInput("Score 1: "));
+				st.SetScore1(score1);
+
+				int score2 = int.Parse(GetInput("Score 2: "));
+				st.SetScore2(score2);
+
+				int score3 = int.Parse(GetInput("Score 3: "));
+				st.SetScore3(score3);
+
+				Console.WriteLine("\n" + st.GetSummary());
+
+				doAnother = GetInput("\nDo another (y/n): ");
+
+			} while (doAnother == "y");
+
+			Console.WriteLine("\nThank you!");
+		}
+
+		private static string GetInput(string prompt)
+		{
+			Console.Write(prompt);
+			return Console.ReadLine();
+		}
+	}
+}
---
+++ StaticClassDemo/Student.cs
@@ -0,0 +1,113 @@
+﻿namespace StaticClassDemo
+	{
+		class Student
+		{
+			public static string[] majors = new string[] { "Programming", "Business", "Art History" };
+
+			private string major;
+
+			public string GetMajor()
+			{
+				return major;
+			}
+
+			public void SetMajor(string major)
+			{
+				this.major = major;
+			}
+
+			private int score1;
+
+			public int GetScore1()
+			{
+				return score1;
+			}
+
+			public void SetScore1(int score1)
+			{
+				this.score1 = score1;
+				Calc();
+			}
+
+			private int score2;
+
+			public int GetScore2()
+			{
+				return score2;
+			}
+
+			public void SetScore2(int score2)
+			{
+				this.score2 = score2;
+				Calc();
+			}
+
+			private int score3;
+
+			public int GetScore3()
+			{
+				return score3;
+			}
+
+			public void SetScore3(int score3)
+			{
+				this.score3 = score3;
+				Calc();
+			}
+
+			private string firstName;
+
+			public string GetFirstName()
+			{
+				return firstName;
+			}
+
+			public void SetFirstName(string firstName)
+			{
+				this.firstName = firstName;
+			}
+
+			private string lastName;
+
+			public string GetLastName()
+			{
+				return lastName;
+			}
+
+			public void SetLastName(string lastName)
+			{
+				this.lastName = lastName;
+			}
+
+			private string studentNumber;
+
+			public string GetStudentNumber()
+			{
+				return studentNumber;
+			}
+
+			public void SetStudentNumber(string num)
+			{
+				studentNumber = num;
+			}
+
+			//Read only attributes
+			private float average;
+
+			public float GetAverage()
+			{
+				return average;
+			}
+
+			public string GetSummary()
+			{
+				return firstName + " " + lastName + " " + studentNumber + " Average: " + average;
+			}
+
+			//Private methods
+			private void Calc()
+			{
+				average = (score1 + score2 + score3) / 3;
+			}
+		}
+}
